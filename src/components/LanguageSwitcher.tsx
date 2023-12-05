0c5339522333bfe168e71a1da84ec2e2d4a7d601
Mohamed Mukhtar <mukxdev@gmail.com>
2023-12-05 16:40:07 +0300

    Added language switcher

---
+++ src/components/LanguageSwitcher.tsx
@@ -0,0 +1,73 @@
+"use client";
+
+import React, { FC } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { Globe } from "lucide-react";
+
+import { usePathname, useParams, useRouter } from "next/navigation";
+import { availableLocales } from "@/config";
+import { useTranslations } from "next-intl";
+
+export const LanguageSwitcher: FC = () => {
+  const t = useTranslations("Home");
+
+  const path = usePathname();
+  const params = useParams();
+  const router = useRouter();
+
+  /**
+   * The function `changeLanguage` takes a locale as input and updates the path in the router to include
+   * the new locale if it matches one of the supported locales.
+   * @param {string} locale - The `locale` parameter is a string that represents the desired language or
+   * locale to change to.
+   */
+  function changeLanguage(locale: string) {
+    const regex = new RegExp(`^/(${availableLocales.join("|")})`);
+    const matchedLocale = path.match(regex);
+    if (matchedLocale) {
+      const updatedPath = path.replace(matchedLocale[0], `/${locale}`);
+      router.push(updatedPath);
+    }
+  }
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size={"icon"} className="border-primary">
+          <Globe />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent className="w-56 hover:bg-white flex flex-col">
+        <DropdownMenuLabel>{t("language")}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        {availableLocales.map((l, i) => {
+          let languageNames = new Intl.DisplayNames([l], { type: "language" });
+
+          return (
+            <DropdownMenuItem asChild key={i}>
+              <Button
+                variant={"ghost"}
+                disabled={l === params.locale}
+                onClick={() => changeLanguage(l)}
+                className={cn(
+                  "w-full justify-start menuitem disabled:bg-primary disabled:opacity-100 disabled:text-primary-foreground"
+                )}
+              >
+                {languageNames.of(l)}
+              </Button>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
--- src/components/user/UserProfilePreview.tsx
+++ src/components/user/UserProfilePreview.tsx
@@ -2,11 +2,11 @@ import { TUser } from "@/lib/types";
 import React, { FC } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { useTranslations } from "next-intl";
+import { LanguageSwitcher } from "@/components/LanguageSwitcher";
 
 type TUserProfilePreview = {
   user: TUser;
 };
-
 
 export const UserProfilePreview: FC<TUserProfilePreview> = ({ user }) => {
   const t = useTranslations("Dashboard");
@@ -14,17 +14,20 @@ export const UserProfilePreview: FC<TUserProfilePreview> = ({ user }) => {
   return (
     <div className="p-6 text-primary rounded-lg flex flex-col gap-2">
       {/* profile avatar */}
-      <div className="flex gap-3 items-center">
-        <Avatar className="">
-          {user.avatar && <AvatarImage src={user.avatar} />}
-          <AvatarFallback className="text-primary-foreground bg-primary">
-            {user.name.slice(0, 2)}
-          </AvatarFallback>
-        </Avatar>
-        <div>
-          <h2>{t("hello")}</h2>
-          <h2 className="text-xl font-semibold">{user.name}</h2>
+      <div className="flex justify-between gap-4 items-center">
+        <div className="flex gap-3 items-center">
+          <Avatar className="">
+            {user.avatar && <AvatarImage src={user.avatar} />}
+            <AvatarFallback className="text-primary-foreground bg-primary">
+              {user.name.slice(0, 2)}
+            </AvatarFallback>
+          </Avatar>
+          <div>
+            <h2>{t("hello")}</h2>
+            <h2 className="text-xl font-semibold">{user.name}</h2>
+          </div>
         </div>
+        <LanguageSwitcher />
       </div>
     </div>
   );
