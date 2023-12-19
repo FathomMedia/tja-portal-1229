5babea528532c69322b8d2cc0bbb99994d5beb2c
Mohamed Mukhtar <mukxdev@gmail.com>
2023-12-19 09:29:02 +0300

    update api url

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export const availableLocales = ["en", "ar"];
 export const timezone = "Asia/Bahrain";
 
 // ? Production
-// export const api = "http://157.245.210.179/api";
+export const api = "http://157.245.210.179/api";
 // ? Locale
-export const api = "http://v20s-imac.local:8000/api";
+// export const api = "http://v20s-imac.local:8000/api";
