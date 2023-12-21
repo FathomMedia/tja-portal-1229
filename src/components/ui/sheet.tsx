41dbb730100830a403feaaac3e1216da20d365e6
Mohamed Mukhtar <mukxdev@gmail.com>
2023-12-21 11:28:54 +0300

    added sheet size

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -40,13 +40,19 @@ const sheetVariants = cva(
         top: "inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top",
         bottom:
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
-        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
+        left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left",
         right:
-          "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+          "inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right",
+      },
+      size: {
+        default: "sm:max-w-sm",
+        md: "sm:max-w-lg",
+        lg: "sm:max-w-2xl",
       },
     },
     defaultVariants: {
       side: "right",
+      size: "default",
     },
   }
 );
@@ -58,7 +64,7 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => {
+>(({ side = "right", size, className, children, ...props }, ref) => {
   const locale = useLocale();
   return (
     <SheetPortal>
@@ -66,7 +72,7 @@ const SheetContent = React.forwardRef<
       <SheetPrimitive.Content
         ref={ref}
         dir={isRtlLang(locale) ? "rtl" : "ltr"}
-        className={cn(sheetVariants({ side }), className)}
+        className={cn(sheetVariants({ side, size }), className)}
         {...props}
       >
         {children}
