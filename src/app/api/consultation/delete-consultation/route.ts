e998824f671ec240f054032d91cae6bda0bb825e
Sarah P <sarah.p@fthm.me>
2024-01-07 09:48:22 +0300

    added delete consultation package

---
+++ src/app/api/consultation/delete-consultation/route.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { apiReq } from "@/lib/apiHelpers";
+
+export async function POST(request: NextRequest) {
+  const data = await request.json();
+  const cookieStore = cookies();
+  const token = cookieStore.get("authToken");
+
+  return await apiReq({
+    endpoint: `/consultations/${data.id}`,
+    locale: request.headers.get("Accept-Language") || "en",
+    method: "DELETE",
+    token: token?.value,
+  }).then(async (value) => {
+    if (value.ok) {
+      const data = await value.json();
+      return NextResponse.json(data, { status: 200 });
+    }
+    const data = await value.json();
+    return NextResponse.json(data, { status: value.status });
+  });
+}
