c4830b3661aefb489efdc8b716d2f0d9b59cc5d9
Mohamed Mukhtar <mukxdev@gmail.com>
2024-01-07 16:39:37 +0300

    attach revoke achievements from customers

---
+++ src/app/api/user/achievement/remove/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { apiReq } from "@/lib/apiHelpers";
+
+export async function POST(request: NextRequest) {
+  const cookieStore = cookies();
+  const token = cookieStore.get("authToken");
+  const locale = request.headers.get("accept-language") ?? "en";
+
+  const data: {
+    customerId: string;
+    achievementId: string;
+  } = await request.json();
+
+  const suspendRes = await apiReq({
+    endpoint: `/customers/${data.customerId}/achievements/${data.achievementId}`,
+    locale,
+    method: "DELETE",
+    token: token?.value,
+  })
+    .then(async (res) => {
+      return res;
+    })
+    .catch((error) => {
+      return NextResponse.json(error, { status: 500 });
+    });
+
+  return suspendRes;
+}
