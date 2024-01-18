88c51b9e6bf9728beca79b33b1e36abc9749f113
Mohamed Mukhtar <mukxdev@gmail.com>
2024-01-18 14:44:04 +0300

    ..

--- src/app/[locale]/authentication/layout.tsx
+++ src/app/[locale]/authentication/layout.tsx
@@ -7,7 +7,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   const locale = useLocale();
   return (
     <div className="flex flex-col h-[100svh]">
-      <div className="flex z-10 sticky top-0 justify-between items-center h-20 bg-white px-6 py-4 shadow-lg shadow-accent">
+      <div className="flex z-30 sticky top-0 justify-between items-center h-20 bg-white px-6 py-4 shadow-lg ">
         <div className="w-10 sm:block hidden"></div>
         <div className="relative h-full aspect-[3/1] ">
           <Link className="h-full" href={`/${locale}`}>
