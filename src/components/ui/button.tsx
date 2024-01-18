ecebb7b00061925461556414957bd6af9f294dea
Mohamed Mukhtar <mukxdev@gmail.com>
2024-01-18 11:43:35 +0300

    ..

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -21,11 +21,11 @@ const buttonVariants = cva(
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
-        default: "h-10 px-4 py-2",
+        default: "h-11 sm:h-10 px-4 py-2",
         xs: "h-7 rounded-sm px-2 text-xs",
         sm: "h-9 rounded-md px-3",
         lg: "h-11 rounded-md px-8",
-        icon: "h-10 w-10",
+        icon: "h-11 sm:h-10 h-11 sm:w-10",
       },
     },
     defaultVariants: {
